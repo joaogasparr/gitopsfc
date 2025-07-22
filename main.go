59a3c0e43c31f12aceba44b939fbcef54f5bc3a8
João Vitor Gaspar <joao.vgasparr@gmail.com>
2025-07-22 19:23:22 -0300

    first commit

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
+		w.Write([]byte("Hello, Argo!"))
 	})
 
 	http.ListenAndServe(":8080", nil)
